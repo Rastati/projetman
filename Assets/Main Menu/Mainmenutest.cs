9acb37fa2c10322752ea88e65751746ef6b00d52
ViZiOFR <touktouk@projetman.eu>
2019-01-29 15:55:27 +0100

    Merge menus onto new map

---
+++ Assets/Main Menu/Mainmenutest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mainmenutest : MonoBehaviour {
+
+    public string LoadLevel;
+
+    public void StartGame() {
+
+        Application.LoadLevel  (LoadLevel);
+    }
+
+    // Update is called once per frame
+    public void quitGame() {
+
+        Application.Quit();
+    }
+}
---
+++ Assets/Main Menu/SelectLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectLevel : MonoBehaviour
+{
+
+    public string LoadLevel;
+
+    public void StartGame()
+    {
+
+        Application.LoadLevel(LoadLevel);
+    }
+}
